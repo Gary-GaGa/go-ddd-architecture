5c8f88720874105651548f5617e1bad64280e63c
GaryGaGa-TYR <gary201101@tyr-tech.com>
2023-06-02 19:01:53 +0800

    WIP(custom): None
    None

--- app/usecase/query/member/new.go
+++ app/usecase/query/member/new.go
@@ -1,11 +1,15 @@
 // Package query -
 package query
 
+import (
+	in "go-ddd-architecture/app/usecase/port/in/query"
+)
+
 // usecase -
 type usecase struct {
 }
 
 // New -
-func New() {
-
+func New() in.QueryUsecase {
+	return &usecase{}
 }
